636b5a59e10cb098964b1695edfcaff30af2b8b7
Fabian Gröger <fabian.groeger@bluewin.ch>
2019-04-14 09:02:57 +0200

    added renaming of extracting images

--- data_extractor/extract_data.py
+++ data_extractor/extract_data.py
@@ -4,8 +4,12 @@ from data_extractor.extractor import Extractor
 def main():
     print('start extracting regions of interest from data')
     extractor = Extractor()
-    extractor.extract_data()
+    #extractor.extract_data()
     print('finished extracting regions of interest')
+
+    print('started renaming images in categories')
+    extractor.rename_images_in_categories()
+    print('finished renaming images')
 
 
 if __name__ == "__main__":
--- data_extractor/extractor.py
+++ data_extractor/extractor.py
@@ -11,6 +11,7 @@ class Extractor:
         self.CATEGORIES = ["-1", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
         self.isolator = Isolator()
+        self.current_working_dir = os.getcwd()
 
     def set_data_dir(self, data_dir):
         self.DATA_DIR = data_dir
@@ -19,12 +20,10 @@ class Extractor:
         self.CATEGORIES = categories
 
     def extract_data(self):
-        current_working_dir = os.getcwd()
-
-        input_dir = current_working_dir + self.DATA_DIR
+        input_dir = self.current_working_dir + self.DATA_DIR
         print('Input directory: ', input_dir)
 
-        output_dir = current_working_dir + "/data_extracted"
+        output_dir = self.current_working_dir + "/data_extracted"
         print('Output directory: ', output_dir)
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
@@ -36,7 +35,7 @@ class Extractor:
         for image in list_dir:
             image_name = image
             image_name = image_name.partition('.')[0]
-            file_string = current_working_dir + self.DATA_DIR + "/" + image
+            file_string = self.current_working_dir + "{:s}/{:s}".format(self.DATA_DIR, image)
 
             try:
                 image = cv2.imread(file_string)
@@ -44,7 +43,28 @@ class Extractor:
                 regions_of_interest = self.isolator.get_regions_of_interest(image)
 
                 for index, roi in enumerate(regions_of_interest):
-                    roi_file_name = output_dir + "/" + image_name + "_" + str(index) + ".jpg"
+                    roi_file_name = output_dir + "/{:s}_{:s}.jpg".format(image_name, str(index))
                     cv2.imwrite(roi_file_name, roi)
             except:
                 continue
+
+        print('creating folders for sorting rois in categories')
+        for category in self.CATEGORIES:
+            category_dir = output_dir + "/{:s}".format(category)
+            if not os.path.exists(category):
+                os.makedirs(category_dir)
+
+    def rename_images_in_categories(self):
+        for category in self.CATEGORIES:
+            category_dir = os.path.join(self.current_working_dir, "data_extracted", category)
+            for index, image in enumerate(os.listdir(category_dir)):
+                image_array = cv2.imread(os.path.join(category_dir, image))
+                image_name = "{:s}.jpg".format(str(index))
+                image_path = os.path.join(category_dir, image_name)
+                cv2.imwrite(image_path, image_array)
+
+                # if the name and new name are the same, dont remove image
+                # because otherwise this image would be deleted
+                if image_name != image:
+                    image_path = os.path.join(category_dir, image)
+                    os.remove(image_path)
