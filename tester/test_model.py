9e04c0cc8e58888cf36704872a9b7b3180b1cb3c
Fabian Gröger <fabian.groeger@bluewin.ch>
2019-05-01 09:40:08 +0200

    created possibility to test the model with an array

--- tester/test_model.py
+++ tester/test_model.py
@@ -1,10 +1,15 @@
+import numpy as np
 from tester import Tester
 
 
 def main():
     tester = Tester()
-
-    tester.test_model("frame_number_4.jpg")
+    # test the model with a given image
+    tester.test_model_with_image("frame_number_4.jpg")
+    # test the model with a random color array
+    tester.test_model_with_array(np.random.rand(28, 28, 3))
+    # test the model with a random gray scale array
+    tester.test_model_with_array(np.random.rand(28, 28))
 
 
 if __name__ == "__main__":
--- tester/tester.py
+++ tester/tester.py
@@ -13,15 +13,21 @@ class Tester:
 
         self.isolator = Isolator()
 
-    def _preprocess_image(self, image_array):
-        if constants.USE_GRAYSCALE:
-            image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2GRAY)
+    def __reshape_image(self, image_array):
         resized_image_array = cv2.resize(image_array, (constants.IMG_SIZE, constants.IMG_SIZE))
         reshaped_image = resized_image_array.reshape(-1, constants.IMG_SIZE, constants.IMG_SIZE, constants.DIMENSION)
 
         return reshaped_image
 
-    def test_model(self, image_path):
+    def __preprocess_image(self, image_array):
+        if constants.USE_GRAYSCALE:
+            image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2GRAY)
+
+        reshaped_image = self.__reshape_image(image_array)
+
+        return reshaped_image
+
+    def test_model_with_image(self, image_path):
         image_array = cv2.imread(image_path)
         image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
         regions_of_interest = self.isolator.get_regions_of_interest(image_array)
@@ -30,7 +36,7 @@ class Tester:
             roi = roi_arr[0]
             roi_type = roi_arr[1]
 
-            roi_processed = self._preprocess_image(roi)
+            roi_processed = self.__preprocess_image(roi)
             prediction = self.model.predict([roi_processed])
 
             i = prediction.argmax(axis=1)[0]
@@ -42,3 +48,15 @@ class Tester:
 
             cv2.imshow("ROI", roi)
             cv2.waitKey(0)
+
+    def test_model_with_array(self, image_array):
+        image_processed = self.__reshape_image(image_array)
+        prediction = self.model.predict([image_processed])
+
+        i = prediction.argmax(axis=1)[0]
+        label = constants.CATEGORIES[i]
+
+        print('Prediction: {} ({:.2f}%)'.format(label, prediction[0][i] * 100))
+
+        cv2.imshow("ROI", image_array)
+        cv2.waitKey(0)
