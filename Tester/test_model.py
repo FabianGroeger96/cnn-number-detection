1736aeb250c2df730e7a3c7db3c04380c18d9611
Fabian Gröger <fabian.groeger@bluewin.ch>
2019-05-11 10:02:24 +0200

    refactoring :hammer:

--- Tester/test_model.py
+++ Tester/test_model.py
@@ -9,18 +9,18 @@ def main():
     tester = Tester(ModelGNetDeepV2('GNet'), 'CNN-gnet-deep-v2-new-big-data')
 
     # test the model with a given image
-    # Tester.test_model_with_image('frame_overlaying_cnt_2.jpg')
+    tester.test_model_with_image('frame_overlaying_cnt_2.jpg')
 
     # test the model with a random color array
     # image_random_color = np.random.randint(0, 255, size=(28, 28, 3), dtype=np.uint8)
-    # Tester.test_model_with_array(image_random_color)
+    # tester.test_model_with_array(image_random_color)
 
     # test the model with a random gray scale array
     # image_random_gray = np.random.randint(0, 255, size=(28, 28), dtype=np.uint8)
-    # Tester.test_model_with_array(image_random_gray)
+    # tester.test_model_with_array(image_random_gray)
 
     # test the model with a folder of images
-    tester.test_model_with_folder('continuous', display_all=False)
+    # tester.test_model_with_folder('continuous', display_all=False)
 
     # create video from images command
     # convert *.jpg recognized.mpeg
