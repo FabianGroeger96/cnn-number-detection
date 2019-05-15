9b7d01d2792b1c7d3b1229ccf12a809675041da7
Fabian Gröger <fabian.groeger@bluewin.ch>
2019-05-15 11:24:53 +0200

    refactoring :hammer:

--- constants.py
+++ constants.py
@@ -3,7 +3,6 @@ OUTPUT_DATA_DIR = "data_extracted"
 CATEGORIES = ["-1", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 MODEL_DIR = "../TrainedModels/"
-ASSET_DIR = "../Assets/"
 
 IMG_SIZE = 28
 USE_GRAY_SCALE = True
@@ -11,6 +10,6 @@ DIMENSION = 1
 
 SIGNAL_TYPES = ["info", "stop"]
 
-BATCH_SIZE = 16
-EPOCHS = 15
+BATCH_SIZE = 128
+EPOCHS = 5
 VALIDATION_SPLIT = 0.25
